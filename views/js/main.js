4d1f1d8079d0f57f639f5e344f49ff9dfd44dede
tgem <tim.gemkow@gmx.de>
2017-09-09 18:50:59 +0200

    optimized speed

--- views/js/main.js
+++ views/js/main.js
@@ -422,8 +422,8 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
+  function determineDx (size, oldWidth) {
+    // var oldWidth = elem.offsetWidth;
     var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
     var oldSize = oldWidth / windowWidth;
 
@@ -449,10 +449,13 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzas = document.querySelectorAll(".randomPizzaContainer");
+    var oldWidth = pizzas[0].offsetWidth;
+    var dx = determineDx(size, oldWidth);
+
+    for (var i = 0; i < pizzas.length; i++) {
+      var newWidth = (oldWidth + dx) + 'px';
+      pizzas[i].style.width = newWidth;
     }
   }
 
@@ -496,15 +499,24 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
+
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
+  var scrollTop = document.body.scrollTop;
+  var phases=[];
+  for(var j=0;j<5;j++) {
+    phases[j] = Math.sin((scrollTop / 1250) + j);    
+  }
+
+  var maxX = document.documentElement.cientWidth;
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var phase = phases[i % 5];
+    var left = items[i].basicLeft + 100 * phase + 'px';
+      items[i].style.left = left;      
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
